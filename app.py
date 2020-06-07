cab9b3f8ac5e13cab611327dcfd917df3db7a72a
cgliu-create <chrisliu2438@gmail.com>
2020-06-07 10:06:57 -0400

    editing sched

--- app.py
+++ app.py
@@ -41,11 +41,9 @@ class SchedData:
         # clears current data
         self.data = [list() for i in range(8)]
         self.color_data = {}
-        # reads csv if it exists
-        sched = sched_name+'.csv'
-        if not os.path.exists(sched):
-            return "ERROR: SCHED DOES NOT EXIST"
-        reader = csv.reader(open(sched))
+        # reads csv
+        sched = open(sched_name+'.csv')
+        reader = csv.reader(sched)
         line_num = 0
         for line in reader:
             if line_num < 8:
@@ -64,19 +62,29 @@ class SchedData:
                 item = line[1]
                 self.color_data[color] = item
             line_num += 1
+        sched.close()
+    # given the name of the sched
+    # returns a list representing a line of the csv
+    def readCSVLine(self, sched_name, line_num):
+        self.readCSV(sched_name)
+        return self.data[line_num]
+
     # given the name of the sched and a line
     # either creates a sched or opens an existing sched
     # adds the line
-    def writeCSVLine(self, sched_name, line):
-        sched = sched_name+'.csv'
-        if not os.path.exists(sched):
-            f = open(sched, 'x')
-            f.write(line+'\n')
-            f.close()
+    def writeCSVLine(self, sched_name, line, write_over=False):
+        if not os.path.exists(sched_name+'.csv'):
+            sched = open(sched_name+'.csv', 'x')
+            sched.write(line+'\n')
+            sched.close()
+        elif write_over:
+            sched = open(sched_name+'.csv', 'w')
+            sched.write(line)
+            sched.close()
         else:
-            f = open(sched, 'a')
-            f.write(line+'\n')
-            f.close()
+            sched = open(sched_name+'.csv', 'a')
+            sched.write(line+'\n')
+            sched.close()
 
     # given the name of the sched, the line num in question, and a line
     # copys the content before and after this line num
@@ -92,7 +100,51 @@ class SchedData:
             if num > line_num:
                 lines_after += line_read
             num+=1
-        content = lines_before +  line + lines_after
+        sched.close()
+        content = lines_before +  line + '\n' + lines_after
+        self.writeCSVLine(sched_name, content, True)
+
+        
+class SchedEditor:
+    def __init__(self, sched_data=SchedData()):
+        self.sched_data = sched_data
+        self.day_idex = { 'mon': 1, 'tue': 2, 'wed': 3, 'thu': 4, 'fri': 5, 'sat': 6, 'sun': 7 }
+        self.time_index = {}
+
+    def markTimeIndex(self, sched_name):
+        line0 =self.sched_data.readCSVLine(sched_name, 0)
+        index = 0
+        for mark in line0:
+            self.time_index[mark] = index
+            index += 1
+
+    def markSchedule(self, sched_name, day, times, color):
+        line_num = self.day_idex[day]
+        content = self.sched_data.readCSVLine(sched_name, line_num)
+        check = 24 - len(content)
+        if check > 0:
+            for i in range(check):
+                content.append('white')
+        changes = []
+        for mark in times:
+            changes.append(self.time_index[mark])
+        for spot in changes:
+            content.insert(spot, color)
+            content.pop(spot + 1)
+        line = ""
+        index = 0
+        for mark in content:
+            if index == 0:
+                line += mark
+            else: 
+                line = line + ',' + mark
+            index +=1
+        self.sched_data.editCSVLine(sched_name, line_num, line)
+
+    def markMeaning(self, sched_name, color, meaning):
+        line = color + ',' + meaning
+        self.sched_data.writeCSVLine(sched_name, line, False)
+
 
 # this displays the schedule data based off SchedData
 class SchedDraw():
@@ -160,14 +212,17 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.SchedDraw = SchedDraw(20, 20, 20, 20, 10)
         self.SchedData = SchedData()
+        self.SchedEditor = SchedEditor(self.SchedData)
         sdraw = self.SchedDraw
         self.width = (sdraw.square_size + sdraw.margin) * 28
         self.height = (sdraw.square_size + sdraw.margin) * 12
         self.setGeometry(0, 0, self.width, self.height)
         self.setWindowTitle("Chris Sched")
         self.SchedData.readCSV('empty')
+        self.current_sched = 'empty'
         self.show_color = True
         self.show_scheds = False
+        self.show_instr = False
         self.InitWindow()
     # this creates all the interactable gui elements
     def InitWindow(self):
@@ -180,39 +235,39 @@ class MainWindow(QMainWindow):
         self.txt0.move(x, y)
         x += self.txt0.frameGeometry().width()
     # button controls
-        # get
+    # get
         self.btn0 = QPushButton("get sched", self)
         self.btn0.clicked.connect(self.loadSched)
         self.btn0.move(x, y)
         x += self.btn0.frameGeometry().width()
-        # new
+    # new
         self.btn1 = QPushButton("new sched", self)
         self.btn1.clicked.connect(self.newSched)
         self.btn1.move(x, y)
         x += self.btn1.frameGeometry().width()
-        # delete
+    # delete
         self.btn2 = QPushButton("del sched", self)
         self.btn2.clicked.connect(self.deleteSched)
         self.btn2.move(x, y)
         x += self.btn2.frameGeometry().width()
-        # edit
+    # edit
         self.btn3 = QPushButton("edit sched", self)
-        self.btn3.clicked.connect(self.loadSched)
+        self.btn3.clicked.connect(self.editSched)
         self.btn3.move(x, y)
         x += self.btn3.frameGeometry().width()
-        # colors
+    # colors
         self.btn4 = QPushButton("show colors", self)
         self.btn4.clicked.connect(self.showColors)
         self.btn4.move(x, y)
         x += self.btn4.frameGeometry().width()
-        # scheds
+    # scheds
         self.btn5 = QPushButton("show scheds", self)
         self.btn5.clicked.connect(self.showScheds)
         self.btn5.move(x, y)
         x += self.btn5.frameGeometry().width()
-        # intructions
+    # intructions
         self.btn6 = QPushButton("show instr", self)
-        self.btn6.clicked.connect(self.showColors)
+        self.btn6.clicked.connect(self.showInstr)
         self.btn6.move(x, y)
         y = sdraw.sched_pos_y + 3 * sdraw.square_size + (sdraw.square_size + sdraw.margin) * 8
         x = sdraw.sched_pos_x + 0
@@ -221,13 +276,14 @@ class MainWindow(QMainWindow):
         self.txt1.resize((sdraw.square_size + sdraw.margin) * 27, self.txt1.frameGeometry().height()) 
         self.txt1.move(x, y)
 
-# these are functions called by the button controls
+    # these are functions called by the button controls
     # changes all the show_things to False
     def dontShow(self):
-        self.show_color = self.show_scheds = False
+        self.show_color = self.show_scheds = self.show_instr = False
     # sets show_color to be the only one to be true
     def showColors(self):
         self.dontShow()
+        self.SchedData.readCSV( self.current_sched)
         self.show_color = True
         self.repaint()
     # sets show_sched to be the only one to be true
@@ -240,12 +296,12 @@ class MainWindow(QMainWindow):
     # if this name exists, Sched Data record info of this Sched
     def loadSched(self):
         name = self.txt0.text()
-        sched = name+'.csv'
         self.dontShow()
-        if not os.path.exists(sched):
+        if not os.path.exists(name+'.csv'):
             self.txt1.setText("invalid sched")
         else:
             self.SchedData.readCSV(name)
+            self.current_sched = name
             self.txt1.setText("complete")
         self.repaint()
     # reads the name from command text input
@@ -253,12 +309,11 @@ class MainWindow(QMainWindow):
     # if this name exists, removes the corresponging csv file
     def deleteSched(self):
         name = self.txt0.text()
-        sched = name+'.csv'
         self.dontShow()
-        if not os.path.exists(sched):
+        if not os.path.exists(name+'.csv'):
            self.txt1.setText("invalid sched")
         else:
-            os.remove(sched)
+            os.remove(name+'.csv')
             self.txt1.setText("complete")
         self.repaint()
     # reads the name from command text input
@@ -266,9 +321,8 @@ class MainWindow(QMainWindow):
     # if this name DNE, creates and empty schedule csv file
     def newSched(self):
         name = self.txt0.text()
-        sched = name +'.csv'
         self.dontShow()
-        if os.path.exists(sched):
+        if os.path.exists(name +'.csv'):
             self.txt1.setText("sched name already taken")
         else:
             line0 = ""
@@ -285,13 +339,44 @@ class MainWindow(QMainWindow):
                 self.SchedData.writeCSVLine(name, '\n')
             self.txt1.setText("complete")
         self.repaint()
+
+    # sets show_instr to be the only one to be true
+    def showInstr(self):
+        self.dontShow()
+        self.show_instr = True
+        self.repaint()
+
+    # reads the command from text input
+    # changes the schedule csv file of the currently opened sched
     def editSched(self):
-        pass
+        command = self.txt0.text()
+        self.dontShow()
+        if command.count(';') == 2:
+            # mon;6am,7am,8am,9am,10am,11am,Noon;black
+            name = self.current_sched
+            days = command[0:command.index(';')].split(',')
+            times = command[command.index(';')+1:command.rindex(';')].split(',')
+            color = command[command.rindex(';')+1:]
+            self.SchedEditor.markTimeIndex(name)
+            for day in days:
+                self.SchedEditor.markSchedule(name, day, times, color)
+            self.txt1.setText("complete")
+        elif command.count('*') == 1 and command.count(':') == 1:
+            # *red:meaning
+            name = self.current_sched
+            color = command[1 :command.index(':')]
+            meaning = command[command.index(':')+1:]
+            self.SchedEditor.markMeaning(name, color, meaning)
+        else:
+            self.txt1.setText("invalid command")
+        self.repaint()
+
     # this controls what is displayed
     def paintEvent(self, event):
         painter = QPainter(self)
         self.SchedDraw.drawSched(painter, self.SchedData)
         if self.show_color:
+
             data = self.SchedDraw.getColors(painter, self.SchedData)
             self.txt1.setText(data)
         if self.show_scheds:
@@ -301,6 +386,8 @@ class MainWindow(QMainWindow):
                     scheds.append(entry[0:entry.index('.csv')])
             scheds.remove('empty')
             self.txt1.setText(str(scheds))
+        if self.show_instr:
+            self.txt1.setText("to change colors: mon,tue;6am,7am,8am,9am,10am,11am,Noon;black   to add meaning: *red:meaning")
 
 # testing
 if __name__ == "__main__":
@@ -308,3 +395,4 @@ if __name__ == "__main__":
     window = MainWindow()
     window.show()
     app.exec_()
+# nice
